bcb2959e28892d630ae9b4e4a93d986374d1841f
Mo-UNO <mohamedamir4491@gmail.com>
2023-08-28 18:45:00 -0700

    c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,19 +13,20 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int iteration, je;
 
-iteration = 0;
+	iteration = 0;
+
 	while (s[iteration] != '\0')
 	{
 		for (je = 0; accept[je] != '\0'; je++)
 		{
 			if (s[iteration] == accept[je])
 			{
-			x = &s[iteration];
-	return (x);
+				x = &s[iteration];
+				return (x);
 			}
-je++;
 		}
-iteration++;	
-}
+
+	iteration++;
+	}
 	return (0);
 }
