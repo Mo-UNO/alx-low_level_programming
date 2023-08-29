e2b737b554a8f2843f3f62f0b53734f05eb4e6f6
Mo-UNO <mohamedamir4491@gmail.com>
2023-08-29 01:21:10 -0700

    c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i1;
+	int j1;
+
+	for (i1 = 0; i1 < 8; i1++)
+	{
+		for (j1 = 0; j1 < 8; j1++)
+			_putchar(a[i1][j1]);
+		_putchar('\n');
+	}
+}
+
